84d22a353420ca8ac9279d133e7a87d7d431fbf2
niko-roether <nicholas.roether@t-online.de>
2024-10-07 17:49:50 +0200

    Add proper webextension manifest

--- packages/palantir-webextension/src/+manifest.json.mjs
+++ packages/palantir-webextension/src/+manifest.json.mjs
@@ -1,4 +1,15 @@
 export default (context) => ({
-	test: "123",
-	file: context.include("+popup.pug")
+	manifest_version: 3,
+	name: "Palantir",
+	version: context.version,
+	description: "A browser extension to watch videos together remotely on any website"
+		+ (context.environment == "debug" ? " (DEBUG VERSION)" : ""),
+	permissions: ["tabs", "storage"],
+	action: {
+		default_title: "Palantir",
+		default_popup: context.include("+popup.pug"),
+	},
+	background: {
+		scripts: [context.include("+background.ts")]
+	}
 })
