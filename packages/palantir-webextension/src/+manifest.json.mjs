b73ecb413470e21abd70cf0861bed5b6884193c6
niko-roether <nicholas.roether@t-online.de>
2024-10-07 21:53:22 +0200

    Define CSP in manifest

--- packages/palantir-webextension/src/+manifest.json.mjs
+++ packages/palantir-webextension/src/+manifest.json.mjs
@@ -1,8 +1,21 @@
+const DESCRIPTION_MAP = {
+	"prod": "A browser extension to watch videos together remotely on any website",
+	"debug": "(DEBUG VERSION) A browser extension to watch videos together remotely on any website"
+}
+
+const CONTENT_SECURITY_POLICY_MAP = {
+	"prod": "default-src 'self'; upgrade-insecure-requests;",
+	"debug": "default-src 'self';"
+}
+
+
 export default (context) => ({
 	manifest_version: 3,
 	name: "Palantir",
 	version: context.version,
-	description: "A browser extension to watch videos together remotely on any website"
-		+ (context.environment == "debug" ? " (DEBUG VERSION)" : ""),
+	description: DESCRIPTION_MAP[context.environment],
 	permissions: ["tabs", "storage"],
+	content_security_policy: {
+		extension_pages: CONTENT_SECURITY_POLICY_MAP[context.environment]
+	}
 })
