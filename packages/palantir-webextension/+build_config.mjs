15ec2360b2d8bc702890adc5ce113379e5522f85
niko-roether <nicholas.roether@t-online.de>
2024-10-11 00:28:38 +0200

    Implement checkbox

--- packages/palantir-webextension/+build_config.mjs
+++ packages/palantir-webextension/+build_config.mjs
@@ -1,3 +1,10 @@
-export default (context) => ({
-	exclude: context.target == "firefox" ? ["+webextension-polyfill.ts"] : []
-})
+
+export default function(context) {
+	const exclude = [];
+	if (context.target == "firefox") exclude.push("+webextension-polyfill.ts");
+	if (context.environment != "debug") {
+		exclude.push("pages/ui-preview/+index.pug");
+		exclude.push("pages/ui-preview/+styles.scss");
+	}
+	return { exclude };
+}
