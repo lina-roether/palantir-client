f184f1cc8367f61ae2696e1b35f96e5313efa7ae
niko-roether <nicholas.roether@t-online.de>
2024-10-11 03:18:29 +0200

    Row positioning changes + changed style

--- packages/palantir-webextension/src/fragments/input_row/input_row.ts
+++ packages/palantir-webextension/src/fragments/input_row/input_row.ts
@@ -4,9 +4,19 @@ function init(elem: HTMLElement) {
 	const input = elem.querySelector("input");
 	if (input) {
 		const error = assertTypedElement(".js_palantir-input-row__error", HTMLElement, elem);
-		error.innerText = input.validationMessage;
-		input.addEventListener("input", () => {
+		
+		updateErrorMessage();
+		
+		function updateErrorMessage() {
 			error.innerText = input.validationMessage;
+		}
+
+		input.addEventListener("change", () => {
+			setTimeout(() => { updateErrorMessage() });
+		});
+
+		input.addEventListener("input", () => {
+			setTimeout(() => { updateErrorMessage() });
 		});
 	}
 }
