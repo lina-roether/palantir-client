acb28986828215c23cd91cf83be748390bb37ee7
nicholas-roether <nicholas-roether@t-online.de>
2024-10-15 11:45:29 +0200

    Implement basic connection state management

--- packages/palantir-client/src/connection.ts
+++ packages/palantir-client/src/connection.ts
@@ -1,9 +1,62 @@
-import { MessageChannel } from "./messages";
+import log from "@just-log/core";
+import { Message, MessageChannel } from "./messages";
+import { TypedEvent, TypedEventTarget } from "./utils";
 
-export class Connection {
+const logger = log.sub("connection");
+
+export const enum ConnectionState {
+	INITIAL,
+	CONNECTED,
+	AUTHENTICATED,
+	DISCONNECTED
+}
+
+export class StateChangeEvent extends TypedEvent<"statechange"> {
+	constructor(public readonly state: ConnectionState) {
+		super("statechange");
+	}
+}
+
+interface ConnectionEventMap {
+	"statechange": StateChangeEvent
+}
+
+export class Connection extends TypedEventTarget<ConnectionEventMap> {
 	private readonly channel: MessageChannel;
+	private state: ConnectionState = ConnectionState.INITIAL;
 
 	constructor(url: string | URL) {
+		super();
 		this.channel = new MessageChannel(url);
+		this.channel.addEventListener("open", () => {
+			this.onChannelOpen();
+		});
+		this.channel.addEventListener("closed", () => {
+			this.onChannelClosed();
+		});
+		this.channel.addEventListener("message", (evt) => {
+			this.onChannelMessage(evt.message);
+		});
+	}
+
+	public getState(): ConnectionState {
+		return this.state;
+	}
+
+	private setState(state: ConnectionState) {
+		this.state = state;
+		this.dispatchEvent(new StateChangeEvent(state));
+	}
+
+	private onChannelOpen() {
+		this.setState(ConnectionState.CONNECTED);
+	}
+
+	private onChannelClosed() {
+		this.setState(ConnectionState.DISCONNECTED);
+	}
+
+	private onChannelMessage(message: Message) {
+		logger.debug(`Received message: ${JSON.stringify(message)}`);
 	}
 }
