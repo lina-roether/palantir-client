a0ed76fbfe3e024bf2591e63f7a3f7763a1702fa
niko-roether <nicholas.roether@t-online.de>
2024-10-17 22:13:42 +0200

    Fix popup page

--- packages/palantir-webextension/src/pages/popup/+popup.ts
+++ packages/palantir-webextension/src/pages/popup/+popup.ts
@@ -1,39 +1,41 @@
+import { getConfig } from "../../config";
 import { baseLogger } from "../../logger";
 import { initComponent } from "../../utils/component";
-import { assertTypedElement } from "../../utils/query";
+import { initStateContainer } from "../../utils/state";
 
 const logger = baseLogger.sub("page", "popup");
-
-const content = assertTypedElement("#popup__content", HTMLElement);
-const incompleteConfigTemplate = assertTypedElement("#popup__template-options-incomplete", HTMLTemplateElement);
-const startSessionTemplate = assertTypedElement("#popup__template-start-session", HTMLTemplateElement);
 
 const enum State {
 	INCOMPLETE_CONFIG,
 	START_SESSION
 }
 
-function setState(state: State) {
-	let template;
-	switch (state) {
-		case State.INCOMPLETE_CONFIG:
-			template = incompleteConfigTemplate;
-			break;
-		case State.START_SESSION:
-			template = startSessionTemplate;
+const stateController = initStateContainer("#popup__content", {
+	[State.INCOMPLETE_CONFIG]: "#popup__template-options-incomplete",
+	[State.START_SESSION]: "#popup__template-start-session"
+});
+
+async function setInitialState() {
+	const config = await getConfig();
+	if (!config.username || !config.username) {
+		stateController.setState(State.INCOMPLETE_CONFIG);
+	} else {
+		stateController.setState(State.START_SESSION)
 	}
-	content.innerHTML = "";
-	content.append(template.content.cloneNode(true));
 }
 
+void setInitialState();
 
-initComponent(".js_popup-open-options", HTMLButtonElement, (button) => {
+
+initComponent(".js_popup__open-options", HTMLButtonElement, (button) => {
 	button.addEventListener("click", () => {
+		logger.info("Opening options page...");
 		void browser.runtime.openOptionsPage();
+		window.close();
 	});
 });
 
-initComponent(".js_popup-start-session", HTMLButtonElement, (button) => {
+initComponent(".js_popup__start-session", HTMLButtonElement, (button) => {
 	button.addEventListener("click", () => {
 		logger.debug("Attempting to start session...");
 		void browser.runtime.sendMessage({ type: "start_session" });
