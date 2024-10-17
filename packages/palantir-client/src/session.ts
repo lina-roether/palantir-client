8eaf30172b4e3e994c415811527d959c778301da
niko-roether <nicholas.roether@t-online.de>
2024-10-18 00:41:05 +0200

    Implement room joining, creating and leaving in session

--- packages/palantir-client/src/session.ts
+++ packages/palantir-client/src/session.ts
@@ -1,21 +1,65 @@
 import { ClosedEvent, Connection, ConnectionOptions, ErrorEvent } from "./connection";
 import { baseLogger } from "./logger";
-import { Message } from "./messages";
+import { Message, RoomStateMsgBody } from "./messages";
 import { TypedEvent, TypedEventTarget } from "./utils";
 
 const logger = baseLogger.sub("session");
 
+export type UserRole = "host" | "guest";
+
+export interface User {
+	id: string,
+	name: string,
+	role: UserRole,
+}
+
+export interface RoomData {
+	id: string,
+	name: string,
+	password: string,
+	users: User[]
+}
+
+export interface SessionState {
+	inRoom: boolean;
+	userRole?: UserRole;
+	roomData?: RoomData;
+}
+
+export class UpdateEvent extends TypedEvent<"update"> {
+	constructor(public readonly state: SessionState) {
+		super("update");
+	}
+}
+
 export interface SessionEventMap {
 	"open": TypedEvent<"open">,
+	"update": UpdateEvent,
 	"closed": ClosedEvent,
 	"error": ErrorEvent,
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface SessionOptions extends ConnectionOptions {}
+export interface SessionOptions extends ConnectionOptions { }
+
+export const enum State {
+	INITIAL,
+	JOINING,
+	CREATING,
+	CREATED,
+	JOINED,
+	LEAVING
+}
+
+export interface RoomInit {
+	name: string,
+	password: string
+}
 
 export class Session extends TypedEventTarget<SessionEventMap> {
 	private readonly connection: Connection;
+	private roomData: RoomData | null = null;
+	private state: State = State.INITIAL;
 
 	constructor(options: SessionOptions) {
 		super();
@@ -38,6 +82,62 @@ export class Session extends TypedEventTarget<SessionEventMap> {
 		return this.connection.open;
 	}
 
+	public isInRoom() {
+		return this.state == State.JOINED || this.state == State.CREATED;
+	}
+
+	public getUserRole(): UserRole | undefined {
+		switch (this.state) {
+			case State.JOINING:
+			case State.JOINED:
+				return "guest";
+			case State.CREATING:
+			case State.CREATED:
+				return "host";
+			default:
+				return undefined;
+		}
+	}
+
+	public getRoomData(): RoomData | null {
+		return this.roomData;
+	}
+
+	public getState(): SessionState {
+		return {
+			inRoom: this.isInRoom(),
+			userRole: this.getUserRole(),
+			roomData: this.roomData ?? undefined
+		}
+	}
+
+	public createRoom(init: RoomInit) {
+		if (this.state != State.INITIAL) {
+			throw new Error("Already in a room");
+		}
+		this.connection.send({
+			m: "room::create/v1",
+			name: init.name,
+			password: init.password
+		})
+		this.state = State.CREATING;
+	}
+
+	public joinRoom(id: string, password: string) {
+		if (this.state != State.INITIAL) {
+			throw new Error("Already in a room");
+		}
+		this.connection.send({ m: "room::join/v1", id, password });
+		this.state = State.JOINING;
+	}
+
+	public leaveRoom() {
+		if (!this.isInRoom()) return;
+		this.roomData = null;
+		this.connection.send({ m: "room::leave/v1" });
+		this.state = State.LEAVING;
+	}
+
 	public close(message: string) {
 		logger.info(`Session closed: ${message}`);
 		this.dispatchEvent(new ClosedEvent(message));
@@ -48,13 +148,104 @@ export class Session extends TypedEventTarget<SessionEventMap> {
 		this.dispatchEvent(new TypedEvent("open"));
 	}
 
+	private requestRoomState() {
+		this.connection.send({ m: "room::request_state/v1" });
+	}
+
+	private broadcastStateUpdate() {
+		this.dispatchEvent(new UpdateEvent(this.getState()));
+	}
+
+	private onRoomJoined() {
+		logger.info(`Successfully joined room`);
+		this.state = State.JOINED;
+		this.broadcastStateUpdate();
+	}
+
+	private onRoomCreated() {
+		logger.info(`Successfully created room`);
+		this.state = State.CREATED;
+		this.requestRoomState();
+		this.broadcastStateUpdate();
+	}
+
+	private onRoomLeft() {
+		logger.info(`Successfully left room`);
+		this.state = State.INITIAL;
+		this.broadcastStateUpdate();
+	}
+
+	private onRoomDisconnected(reason: string) {
+		logger.info(`Room disconnected: ${reason}`);
+		this.state = State.INITIAL;
+		this.dispatchEvent(new ErrorEvent("Room disconnected"));
+		this.broadcastStateUpdate();
+	}
+
+	private updateRoomData(state: RoomStateMsgBody) {
+		this.roomData = {
+			id: state.id,
+			name: state.name,
+			password: state.password,
+			users: state.users.map((user) => ({
+				id: user.id,
+				name: user.name,
+				role: user.role
+			}))
+		}
+		this.broadcastStateUpdate();
+	}
+
+	private onRoomMessage(message: Message) {
+		switch (message.m) {
+			case "room::disconnected/v1":
+				this.onRoomDisconnected(message.reason);
+				break;
+			case "room::state/v1":
+				this.updateRoomData(message);
+				break;
+			default:
+				logger.warning(`Recieved unexpected room message '${message.m}'`);
+		}
+	}
+
 	private onConnectionMessage(message: Message) {
-		logger.debug(`Got session message: ${JSON.stringify(message)}`);
+		switch (this.state) {
+			case State.INITIAL:
+				break;
+			case State.JOINING:
+				if (message.m == "room::join_ack/v1") {
+					this.onRoomJoined();
+					return;
+				}
+				break;
+			case State.CREATING:
+				if (message.m == "room::create_ack/v1") {
+					this.onRoomCreated();
+					return;
+				}
+				break;
+			case State.CREATED:
+			case State.JOINED:
+				this.onRoomMessage(message);
+				return;
+			case State.LEAVING:
+				if (message.m == "room::leave_ack/v1") {
+					this.onRoomLeft();
+					return;
+				}
+		}
+		logger.warning(`Recieved unexpected message '${message.m}'`);
 	}
 
 	private onConnectionError(message: string) {
 		logger.error(`Got error message from server: ${message}`);
 		this.dispatchEvent(new ErrorEvent(message));
+
+		// Interpret error as failing the pending operation
+		if (this.state == State.JOINING || this.state == State.CREATING || this.state == State.LEAVING) {
+			this.state = State.INITIAL;
+		}
 	}
 
 	private onConnectionClosed(message: string) {
