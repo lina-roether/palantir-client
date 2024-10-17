a19aa7aa9f7c08a3d2ca8816426f32c7f9f1d324
nicholas-roether <nicholas-roether@t-online.de>
2024-10-17 16:52:11 +0200

    Add log messages and start_session message

--- packages/palantir-webextension/src/+background.ts
+++ packages/palantir-webextension/src/+background.ts
@@ -5,7 +5,11 @@ import { getConfig, invalidateCachedConfig } from "./config";
 import { baseLogger } from "./logger";
 
 initLogWriter();
+
 const logger = baseLogger.sub("background");
+
+logger.info("Background script started");
+
 type SessionStatus =
 | { status: "active", session: Session }
 | { status: "inactive" }
@@ -14,19 +18,24 @@ type SessionStatus =
 let sessionStatus: SessionStatus = { status: "inactive" };
 
 function startSession(config: SessionOptions) {
-	stopSession();
+	logger.info("Starting new session");
+	stopSession("Superseded by another session");
 	const session = new Session(config);
 	sessionStatus = { status: "active", session };
 }
 
-function stopSession() {
-	if (sessionStatus.status == "active") sessionStatus.session.close("Superseded by another session");
+function stopSession(message: string) {
+	if (sessionStatus.status == "active") {
+		logger.info(`Stopping current session: ${message}`);
+		sessionStatus.session.close(message);
+	}
 }
 
 async function startSessionFromConfig() {
 	const config = await getConfig();
 
 	if (!config.username || !config.serverUrl) {
+		logger.error("Failed to start session: Config is incomplete");
 		sessionStatus = {
 			status: "error",
 			message: "Incomplete configuration. Please use the extension settings and make you have a valid server URL and username set."
@@ -41,10 +50,6 @@ async function startSessionFromConfig() {
 	})
 }
 
-browser.runtime.onStartup.addListener(() => {
-	void startSessionFromConfig()
-});
-
 
 const ConfigChangedRequestSchema = z.object({
 	type: z.literal("config_changed")
@@ -54,22 +59,38 @@ const SessionStatusRequestSchema = z.object({
 	type: z.literal("session_status")
 });
 
-const StopSessionRequestSchema = z.object({
-	type: z.literal("stop_session")
+const StartSessionRequestSchema = z.object({
+	type: z.literal("start_session"),
+	reason: z.string()
 });
 
-const RequestSchema = z.union([ConfigChangedRequestSchema, SessionStatusRequestSchema, StopSessionRequestSchema]);
+const StopSessionRequestSchema = z.object({
+	type: z.literal("stop_session"),
+	reason: z.string()
+});
+
+const RequestSchema = z.union([ConfigChangedRequestSchema, StartSessionRequestSchema, SessionStatusRequestSchema, StopSessionRequestSchema]);
 
 browser.runtime.onMessage.addListener((rawMsg, _sender, sendResponse) => {
-	const message = RequestSchema.parse(rawMsg);
+	let message;
+	try {
+		message = RequestSchema.parse(rawMsg);
+	} catch(e) {
+		logger.error(`Received invalid message: ${e?.toString() ?? "Unknown error"}`);
+		return;
+	}
+	logger.debug(`Received message: ${JSON.stringify(message)}`)
 	switch (message.type) {
 		case "config_changed":
 			invalidateCachedConfig();
+			break;
+		case "start_session":
+			void startSessionFromConfig();
 			break;
 		case "session_status":
 			sendResponse(sessionStatus);
 			break;
 		case "stop_session":
-			stopSession()
+			stopSession(message.reason)
 	}
 })
