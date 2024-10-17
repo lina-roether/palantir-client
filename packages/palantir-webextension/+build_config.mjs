3a3fa378108a23def52b43f8f224fed3eca5ccf6
niko-roether <nicholas.roether@t-online.de>
2024-10-17 22:42:16 +0200

    Properly exclude unneded bundles from different builds

--- packages/palantir-webextension/+build_config.mjs
+++ packages/palantir-webextension/+build_config.mjs
@@ -1,7 +1,13 @@
 
 export default function(context) {
 	const exclude = [];
-	if (context.target == "firefox") exclude.push("+webextension-polyfill.ts");
+	if (context.target == "firefox") {
+		exclude.push("background/+polyfilled.ts");
+		exclude.push("+webextension-polyfill.mjs");
+	}
+	if (context.target == "chromium") {
+		exclude.push("background/+index.ts");
+	}
 	if (context.environment != "debug") {
 		exclude.push("pages/ui-preview/+index.pug");
 		exclude.push("pages/ui-preview/+styles.scss");
